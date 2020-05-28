110bee897bf7d78262aaf14c2efce2156581eccc
ehwah <soewah1996@gmail.com>
2020-05-28 10:16:09 -0400

    Initial Commit

---
+++ ListOfName/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListOfName
+//
+//  Created by sowah on 5/28/20.
+//  Copyright © 2020 sowah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
